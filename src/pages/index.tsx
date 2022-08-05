e5a2a861dc60cf2d437fd873e833130fc2d33876
Krushang Kasundra <kruzkasu223@gmail.com>
2022-08-05 19:55:13 +0530

    feat: added input and button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,37 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
+import { HTMLInputTypeAttribute, useCallback, useState } from "react";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
+  const [url, setUrl] = useState<string>("");
+  const [urlError, setUrlError] = useState<string>("");
+
+  const isUrlValid = (url: string): void => {
+    const urlRegEx =
+      /^(https?:\/\/)?((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|((\d{1,3}\.){3}\d{1,3}))(\:\d+)?(\/[-a-z\d%_.~+]*)*(\?[;&a-z\d%_.~+=-]*)?(\#[-a-z\d_]*)?$/i;
+
+    if (urlRegEx.test(url)) {
+      setUrlError("");
+      return;
+    }
+    setUrlError("please enter valid url");
+  };
+
+  const urlChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value) {
+      isUrlValid(e.target.value);
+      setUrl(e.target.value);
+    } else {
+      setUrlError("");
+      setUrl("");
+    }
+  };
+
+  const onPreviewClick = useCallback(() => {
+    console.log(url);
+  }, [url]);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -16,6 +44,37 @@ const Home: NextPage = () => {
         <img src="/link.svg" alt="link" className={styles.linkIcon} />
         <h1 className={styles.title}>Welcome to Link Preview!</h1>
       </main>
+      <section className={styles.inputSection}>
+        <div className={styles.inputMainWrapper}>
+          <div className={styles.inputWrapper}>
+            <input
+              id="url"
+              type="url"
+              name="url"
+              value={url}
+              className={styles.input}
+              onChange={urlChangeHandler}
+              placeholder="Enter Valid URL"
+              onKeyUp={(e) => {
+                if (
+                  (e.code === "Enter" || e.code === "NumpadEnter") &&
+                  !urlError &&
+                  url
+                )
+                  onPreviewClick();
+              }}
+            />
+            <span className={styles.inputError}>{urlError}</span>
+          </div>
+          <button
+            onClick={onPreviewClick}
+            className={styles.button}
+            disabled={!!urlError || !url}
+          >
+            See Preview
+          </button>
+        </div>
+      </section>
     </div>
   );
 };

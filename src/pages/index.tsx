17afc838f55770c3b800c07c266148cb621fbb2c
Krushang Kasundra <kruzkasu223@gmail.com>
2022-08-06 14:37:40 +0530

    feat: added dynamic data and changed colour theme

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,52 +3,66 @@ import Head from "next/head";
 import { useCallback, useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
 import { IPreviewData } from "../types/types";
+
 const defaultData = {
   oembed: null,
   ogp: {
     "al:android:app_name": ["YouTube"],
     "al:android:package": ["com.google.android.youtube"],
     "al:android:url": [
-      "vnd.youtube://www.youtube.com/watch?v=ybJ_R08SogU&feature=applinks",
+      "vnd.youtube://www.youtube.com/watch?v=5Eqb_-j3FDA\u0026feature=applinks",
     ],
     "al:ios:app_name": ["YouTube"],
     "al:ios:app_store_id": ["544007664"],
     "al:ios:url": [
-      "vnd.youtube://www.youtube.com/watch?v=ybJ_R08SogU&feature=applinks",
+      "vnd.youtube://www.youtube.com/watch?v=5Eqb_-j3FDA\u0026feature=applinks",
     ],
     "al:web:url": [
-      "http://www.youtube.com/watch?v=ybJ_R08SogU&feature=applinks",
+      "http://www.youtube.com/watch?v=5Eqb_-j3FDA\u0026feature=applinks",
     ],
     "fb:app_id": ["87741124305"],
     "og:description": [
-      "An updated version of this live stream will be released shortly. To view this or our other live streams, click the link below.https://www.youtube.com/yellowb...",
+      "Letâs transcend boundaries and bridge distances through compassion, love and identity.#Pasoori #RealMagic #CokeStudioSeason14 #SoundOfTheNationListen on Spot...",
     ],
-    "og:image": ["https://i.ytimg.com/vi/ybJ_R08SogU/maxresdefault_live.jpg"],
+    "og:image": ["https://i.ytimg.com/vi/5Eqb_-j3FDA/maxresdefault.jpg"],
     "og:image:height": ["720"],
     "og:image:width": ["1280"],
     "og:site_name": ["YouTube"],
-    "og:title": [
-      "This live stream has ended. We will release a new version of this live stream soon. See link below",
-    ],
+    "og:title": ["Coke Studio | Season 14 | Pasoori | Ali Sethi x Shae Gill"],
     "og:type": ["video.other"],
-    "og:url": ["https://www.youtube.com/watch?v=ybJ_R08SogU"],
-    "og:video:height": ["360"],
-    "og:video:secure_url": ["https://www.youtube.com/embed/ybJ_R08SogU"],
+    "og:url": ["https://www.youtube.com/watch?v=5Eqb_-j3FDA"],
+    "og:video:height": ["720"],
+    "og:video:secure_url": ["https://www.youtube.com/embed/5Eqb_-j3FDA"],
+    "og:video:tag": [
+      "coke studio",
+      "cocacola pakistan",
+      "coke studio pakistan",
+      "SoundOfTheNation",
+      "Xulfi",
+      "FusionMusic",
+      "Coke Studio Season 14",
+      "Season 14",
+      "Real Magic",
+      "Music",
+      "Coke Studio 14",
+      "Ali Sethi",
+      "Shaegill",
+      "Pasoori",
+      "Shae gill",
+    ],
     "og:video:type": ["text/html"],
-    "og:video:url": ["https://www.youtube.com/embed/ybJ_R08SogU"],
-    "og:video:width": ["640"],
+    "og:video:url": ["https://www.youtube.com/embed/5Eqb_-j3FDA"],
+    "og:video:width": ["1280"],
   },
   seo: {
     description: [
-      "An updated version of this live stream will be released shortly. To view this or our other live streams, click the link below.https://www.youtube.com/yellowb...",
+      "Letâs transcend boundaries and bridge distances through compassion, love and identity.#Pasoori #RealMagic #CokeStudioSeason14 #SoundOfTheNationListen on Spot...",
     ],
     keywords: [
-      "vidÃ©o, partage, tÃ©lÃ©phone-appareil photo, visiophone, gratuit, envoi",
+      "coke studio, cocacola pakistan, coke studio pakistan, SoundOfTheNation, Xulfi, FusionMusic, Coke Studio Season 14, Season 14, Real Magic, Music, Coke Studio 14, Ali Sethi, Shaegill, Pasoori, Shae gill",
     ],
     "theme-color": ["rgba(255, 255, 255, 0.98)"],
-    title: [
-      "This live stream has ended. We will release a new version of this live stream soon. See link below",
-    ],
+    title: ["Coke Studio | Season 14 | Pasoori | Ali Sethi x Shae Gill"],
     "twitter:app:id:googleplay": ["com.google.android.youtube"],
     "twitter:app:id:ipad": ["544007664"],
     "twitter:app:id:iphone": ["544007664"],
@@ -56,34 +70,31 @@ const defaultData = {
     "twitter:app:name:ipad": ["YouTube"],
     "twitter:app:name:iphone": ["YouTube"],
     "twitter:app:url:googleplay": [
-      "https://www.youtube.com/watch?v=ybJ_R08SogU",
+      "https://www.youtube.com/watch?v=5Eqb_-j3FDA",
     ],
     "twitter:app:url:ipad": [
-      "vnd.youtube://www.youtube.com/watch?v=ybJ_R08SogU&feature=applinks",
+      "vnd.youtube://www.youtube.com/watch?v=5Eqb_-j3FDA\u0026feature=applinks",
     ],
     "twitter:app:url:iphone": [
-      "vnd.youtube://www.youtube.com/watch?v=ybJ_R08SogU&feature=applinks",
+      "vnd.youtube://www.youtube.com/watch?v=5Eqb_-j3FDA\u0026feature=applinks",
     ],
     "twitter:card": ["player"],
     "twitter:description": [
-      "An updated version of this live stream will be released shortly. To view this or our other live streams, click the link below.https://www.youtube.com/yellowb...",
+      "Letâs transcend boundaries and bridge distances through compassion, love and identity.#Pasoori #RealMagic #CokeStudioSeason14 #SoundOfTheNationListen on Spot...",
     ],
-    "twitter:image": [
-      "https://i.ytimg.com/vi/ybJ_R08SogU/maxresdefault_live.jpg",
-    ],
-    "twitter:player": ["https://www.youtube.com/embed/ybJ_R08SogU"],
-    "twitter:player:height": ["360"],
-    "twitter:player:width": ["640"],
+    "twitter:image": ["https://i.ytimg.com/vi/5Eqb_-j3FDA/maxresdefault.jpg"],
+    "twitter:player": ["https://www.youtube.com/embed/5Eqb_-j3FDA"],
+    "twitter:player:height": ["720"],
+    "twitter:player:width": ["1280"],
     "twitter:site": ["@youtube"],
     "twitter:title": [
-      "This live stream has ended. We will release a new version of this live stream soon. See link below",
+      "Coke Studio | Season 14 | Pasoori | Ali Sethi x Shae Gill",
     ],
-    "twitter:url": ["https://www.youtube.com/watch?v=ybJ_R08SogU"],
+    "twitter:url": ["https://www.youtube.com/watch?v=5Eqb_-j3FDA"],
   },
-  title:
-    "This live stream has ended. We will release a new version of this live stream soon. See link below - YouTube",
+  title: "Coke Studio | Season 14 | Pasoori | Ali Sethi x Shae Gill - YouTube",
 };
-console.log(defaultData);
+
 const Home: NextPage = () => {
   const [url, setUrl] = useState<string>("");
   const [urlError, setUrlError] = useState<string>("");
@@ -142,85 +153,144 @@ const Home: NextPage = () => {
   }, [previewData]);
 
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Link Preview</title>
-        <meta name="description" content="Generated by create next app" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <>
+      <div className={styles.container}>
+        <Head>
+          <title>Link Preview</title>
+          <meta name="description" content="Generated by create next app" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <main className={styles.main}>
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img src="/link.svg" alt="link" className={styles.linkIcon} />
-        <h1 className={styles.title}>Welcome to Link Preview!</h1>
-      </main>
-      <section className={styles.inputSection}>
-        <div className={styles.inputMainWrapper}>
-          <div className={styles.inputWrapper}>
-            <input
-              id="url"
-              type="url"
-              name="url"
-              value={url}
-              className={styles.input}
-              onChange={urlChangeHandler}
-              placeholder="Enter Valid URL"
-              onKeyUp={(e) => {
-                if (
-                  (e.code === "Enter" || e.code === "NumpadEnter") &&
-                  !urlError &&
-                  url
-                )
-                  onPreviewClick();
-              }}
-            />
-            <span className={styles.inputError}>{urlError}</span>
-          </div>
-          <button
-            onClick={onPreviewClick}
-            className={styles.button}
-            disabled={!!urlError || !url}
-          >
-            See Preview
-          </button>
-        </div>
-      </section>
-      <section className={styles.previewSection}>
-        <div className={styles.previewCard}>
+        <header className={styles.header}>
           {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img
-            className={styles.previewCardImg}
-            src="https://i.ytimg.com/vi/y7MW7d8fb1Y/maxresdefault.jpg"
-            alt="site title"
-          />
-          {/* <embed
-            className={styles.previewCardEmbed}
-            src="https://www.youtube.com/embed/5Eqb_-j3FDA"
-          /> */}
-          <div className={styles.previewCardDetails}>
-            <a
-              target="_blank"
-              rel="noreferrer"
-              className={styles.previewCardLink}
-              href="//example.com"
+          <img src="/link.svg" alt="link" className={styles.linkIcon} />
+          <h1 className={styles.title}>Welcome to Link Previewer!</h1>
+        </header>
+        <section className={styles.inputSection}>
+          <div className={styles.inputMainWrapper}>
+            <div className={styles.inputWrapper}>
+              <input
+                id="url"
+                type="url"
+                name="url"
+                value={url}
+                className={styles.input}
+                onChange={urlChangeHandler}
+                placeholder="Enter Valid URL"
+                onKeyUp={(e) => {
+                  if (
+                    (e.code === "Enter" || e.code === "NumpadEnter") &&
+                    !urlError &&
+                    url
+                  )
+                    onPreviewClick();
+                }}
+              />
+              <span className={styles.inputError}>{urlError}</span>
+            </div>
+            <button
+              onClick={onPreviewClick}
+              className={styles.button}
+              disabled={!!urlError || !url}
             >
-              example.com
-            </a>
-            <a href="//example.com" className={styles.previewCardTitle}>
-              This live stream has ended. We will release a new version of this
-              live stream soon. See link below - YouTube
-            </a>
-            <p className={styles.previewCardDesc}>
-              This live stream has ended. We will release a new version of this
-              live stream soon. See link below - YouTubeAn updated version of
-              this live stream will be released shortly. To view this or our
-              other live streams, click the link
-              below.https://www.youtube.com/yellowb...
-            </p>
+              See Preview
+            </button>
           </div>
-        </div>
-      </section>
-    </div>
+        </section>
+        {previewData || previewDataError ? (
+          <section className={styles.previewSection}>
+            <div className={styles.previewCard}>
+              {previewDataError ? (
+                <div className={styles.previewCardError}>
+                  <p>Oops!</p>
+                  <p>something went wrong</p>
+                  <p>please try again later</p>
+                </div>
+              ) : (
+                <>
+                  {previewData?.ogp?.["og:video:url"]?.[0] ||
+                  previewData?.ogp?.["og:video:secure_url"]?.[0] ||
+                  previewData?.seo?.["twitter:player"]?.[0] ? (
+                    <embed
+                      className={styles.previewCardImg}
+                      src={
+                        previewData?.ogp?.["og:video:url"]?.[0] ||
+                        previewData?.ogp?.["og:video:secure_url"]?.[0] ||
+                        previewData?.seo?.["twitter:player"]?.[0]
+                      }
+                    />
+                  ) : (
+                    <>
+                      {/* eslint-disable-next-line @next/next/no-img-element */}
+                      <img
+                        className={styles.previewCardImg}
+                        src={
+                          previewData?.ogp?.["og:image"]?.[0] ||
+                          previewData?.seo?.["twitter:image"]?.[0] ||
+                          "/placeholder.png"
+                        }
+                        alt={
+                          previewData?.title ||
+                          previewData?.ogp?.["og:title"]?.[0] ||
+                          previewData?.seo?.title?.[0] ||
+                          previewData?.seo?.["twitter:title"]?.[0] ||
+                          "Title"
+                        }
+                      />
+                    </>
+                  )}
+                  <div className={styles.previewCardDetails}>
+                    <a
+                      target="_blank"
+                      href={"//" + url}
+                      rel="noopener noreferrer"
+                      className={styles.previewCardLink}
+                    >
+                      {previewData?.ogp?.["og:site_name"]?.[0] ||
+                        previewData?.seo?.["twitter:site"]?.[0] ||
+                        url}
+                    </a>
+                    <a
+                      target="_blank"
+                      href={"//" + url}
+                      rel="noopener noreferrer"
+                      className={styles.previewCardTitle}
+                    >
+                      {previewData?.title ||
+                        previewData?.ogp?.["og:title"]?.[0] ||
+                        previewData?.seo?.title?.[0] ||
+                        previewData?.seo?.["twitter:title"]?.[0] ||
+                        "Title"}
+                    </a>
+                    <p className={styles.previewCardDesc}>
+                      {previewData?.ogp?.["og:description"]?.[0] ||
+                        previewData?.seo?.description?.[0] ||
+                        previewData?.seo?.["twitter:description"]?.[0] ||
+                        "Description"}
+                    </p>
+                  </div>
+                </>
+              )}
+            </div>
+          </section>
+        ) : (
+          ""
+        )}
+      </div>
+      <footer className={styles.footer}>
+        <p className={styles.footerText}>
+          Made by{" "}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://thekruz.tech"
+            className={styles.footerLink}
+          >
+            Kru'Z
+          </a>
+        </p>
+      </footer>
+    </>
   );
 };
 
